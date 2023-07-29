587ac190c0a7a3fca8001ef91421c933f24274cd
2023-FL <57984642+2023-FL@users.noreply.github.com>
2023-07-29 19:00:15 +0800

    Delete http-common.js

--- http-common.js
+++
@@ -1,9 +0,0 @@
-//const url = "https://pcpdfilm.starsknights.com:18888/api/v2"
-//const url = "https://pcpdfilm.starsknights.com:18888/"
-//new updated url on 10th June, 2023
-const url = "https://pcpdfilm.starsknights.com:18989/api/v2"
-/*It may create a json dictionary to call many url
-e.g. url = {
-  a: "https://pcpdfilm.starsknights.com:18888/api/v2"
-}*/
-export default url
