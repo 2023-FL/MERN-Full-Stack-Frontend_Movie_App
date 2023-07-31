278e3cc5cd921dddb77c8486c0c518874f5a2b8b
2023-FL <57984642+2023-FL@users.noreply.github.com>
2023-08-01 00:50:16 +0800

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
-//const url = "https://pcpdfilm.starsknights.com:18888/api/v2"
-//const url = "https://pcpdfilm.starsknights.com:18888/"
-//new updated url on 10th June, 2023
-const url = "https://pcpdfilm.starsknights.com:18989/api/v2"
+//Please try to look for an appropriate free movie API, such as https://rapidapi.com/juanroldan1989/api/moviequotes/ 
+//to register a user account and get a free API key, e.g. 'https://juanroldan1989-moviequotes-v1.p.rapidapi.com/api/v1/quotes'
+const url = "https://xxxxFREE_MOVIE_API"
+
 /*It may create a json dictionary to call many url
 e.g. url = {
   a: "https://pcpdfilm.starsknights.com:18888/api/v2"
